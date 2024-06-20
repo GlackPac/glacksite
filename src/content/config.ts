2d5d4118100db968025a9ca14c6ac281cf470ce4
GlackPac <mylesglacken@gmail.com>
2024-06-20 17:40:29 -0400

    not doing version control properly lol - changed posts to content collection and added image to blog front matter instead of mdx import

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      tags: z.array(z.string()),
+      image: image()
+    })
+});
+const galleryCollection = defineCollection ({
+  type: 'data',
+  schema: z.object({
+  })
+})
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("posts")
   return rss({
     title: 'Myles G | Astro Tutorial Blog',
-    description: 'My journey learning Astro',
+    description: 'My journey learning Astro and myself',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.mdx')),
+    items: posts.map((post) => {
+      title: post.data.title
+      pubDate: post.data.pubDate
+      description: post.data.description
+      link: `/posts/${post.slug}`
+    }),
     customData: `<language>en-us</language>`,
   });
 }
